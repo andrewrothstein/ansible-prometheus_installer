cbd06035bfd6cbc2cad9d66835d7c0fc13a3a172
Andrew Rothstein <andrew.rothstein@gmail.com>
2020-01-02 09:03:17 -0500

    up to prometheus 2.15.1, alertmanager 0.6.0, consul_exporter 0.6.0, pushgateway 1.0.1, and statsd_exporter 0.13.0

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -36,14 +36,14 @@ dlapp()
     done
 }
 
-dlapp prometheus 2.14.0
-#dlapp alertmanager 0.19.0
-dlapp blackbox_exporter 0.16.0
-#dlapp consul_exporter 0.5.0
+dlapp prometheus 2.15.1
+dlapp alertmanager 0.20.0
+#dlapp blackbox_exporter 0.16.0
+dlapp consul_exporter 0.6.0
 #dlapp graphite_exporter 0.6.2
 #dlapp haproxy_exporter 0.10.0
 #dlapp memcached_exporter 0.6.0
 #dlapp mysqld_exporter 0.12.1
 #dlapp node_exporter 0.18.1
-#dlapp pushgateway 1.0.0
-#dlapp statsd_exporter 0.12.2
+dlapp pushgateway 1.0.1
+dlapp statsd_exporter 0.13.0
