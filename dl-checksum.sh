9b5cf4fb0a7048bc43702786fe5e5040219982cb
Andrew Rothstein <andrew.rothstein@gmail.com>
2021-01-06 13:18:31 -0500

    drop gentoo. add bind_exporter as first community binary. create more useful directory and executable links in /usr/local and /usr/local/bin

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env sh
+set -e
 DIR=~/Downloads
-MIRROR=https://github.com/prometheus
+MIRROR=https://github.com
 
 # https://github.com/prometheus/prometheus/releases/download/v2.11.1/prometheus-2.11.1.darwin-amd64.tar.gz
 # https://github.com/prometheus/alertmanager/releases/download/v0.18.0/alertmanager-0.18.0.darwin-amd64.tar.gz
+# 
 
 dl()
 {
-    local app=$1
-    local ver=$2
-    local os=$3
-    local arch=$4
-    local file=${app}-${ver}.${os}-${arch}.tar.gz
-    local url=$MIRROR/$app/releases/download/v${ver}/$file
-    local lfile=$DIR/$file
+    local ghuser=$1
+    local app=$2
+    local ver=$3
+    local os=$4
+    local arch=$5
+    local platform="${os}-${arch}"
+    local file="${app}-${ver}.${platform}.tar.gz"
+    local url="$MIRROR/$ghuser/$app/releases/download/v${ver}/$file"
+    local lfile="$DIR/$file"
 
     if [ ! -e $lfile ];
     then
@@ -26,24 +30,26 @@ dl()
 
 dlapp()
 {
-    local app=$1
-    local ver=$2
+    local ghuser=$1
+    local app=$2
+    local ver=$3
     printf "  %s:\n" $app
     printf "    '%s':\n" $ver
     for os in darwin linux windows
     do
-        dl $app $ver $os amd64
+        dl $ghuser $app $ver $os amd64
     done
 }
 
-dlapp prometheus 2.23.0
-#dlapp alertmanager 0.21.0
-#dlapp blackbox_exporter 0.18.0
-#dlapp consul_exporter 0.7.1
-#dlapp graphite_exporter 0.9.0
-#dlapp haproxy_exporter 0.11.0
-#dlapp memcached_exporter 0.7.0
-#dlapp mysqld_exporter 0.12.1
-#dlapp node_exporter 1.0.1
-#dlapp pushgateway 1.3.0
-#dlapp statsd_exporter 0.18.0
+dlapp prometheus prometheus 2.23.0
+#dlapp prometheus alertmanager 0.21.0
+#dlapp prometheus blackbox_exporter 0.18.0
+#dlapp prometheus consul_exporter 0.7.1
+#dlapp prometheus graphite_exporter 0.9.0
+#dlapp prometheus haproxy_exporter 0.11.0
+#dlapp prometheus memcached_exporter 0.7.0
+#dlapp prometheus mysqld_exporter 0.12.1
+#dlapp prometheus node_exporter 1.0.1
+#dlapp prometheus pushgateway 1.3.0
+#dlapp prometheus statsd_exporter 0.18.0
+dlapp prometheus-community bind_exporter 0.3.0
