771193834891c1f17086762d75929ddf5c0e5fc1
Andrew Rothstein <andrew.rothstein@gmail.com>
2021-03-14 19:45:46 -0400

    prometheus:2.25.1 node_exporter:1.1.2 pushgateway:1.4.0 statsd_exporter:0.20.0 bind_exporter:0.4.0

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -5,7 +5,6 @@ MIRROR=https://github.com
 
 # https://github.com/prometheus/prometheus/releases/download/v2.11.1/prometheus-2.11.1.darwin-amd64.tar.gz
 # https://github.com/prometheus/alertmanager/releases/download/v0.18.0/alertmanager-0.18.0.darwin-amd64.tar.gz
-# 
 
 dl()
 {
@@ -41,7 +40,7 @@ dlapp()
     done
 }
 
-dlapp prometheus prometheus 2.25.0
+dlapp prometheus prometheus 2.25.1
 #dlapp prometheus alertmanager 0.21.0
 #dlapp prometheus blackbox_exporter 0.18.0
 #dlapp prometheus consul_exporter 0.7.1
@@ -49,7 +48,7 @@ dlapp prometheus prometheus 2.25.0
 #dlapp prometheus haproxy_exporter 0.12.0
 #dlapp prometheus memcached_exporter 0.8.0
 #dlapp prometheus mysqld_exporter 0.12.1
-#dlapp prometheus node_exporter 1.0.1
-#dlapp prometheus pushgateway 1.3.1
-#dlapp prometheus statsd_exporter 0.18.0
-#dlapp prometheus-community bind_exporter 0.3.0
+dlapp prometheus node_exporter 1.1.2
+dlapp prometheus pushgateway 1.4.0
+dlapp prometheus statsd_exporter 0.20.0
+dlapp prometheus-community bind_exporter 0.4.0
