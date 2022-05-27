85c157094c51bb0edbdfcf51e9803f0c3dd8d7ab
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-05-27 18:17:48 -0400

    pushgateway:0.12.1 statsd_exporter:0.22.5

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -42,15 +42,15 @@ dlapp()
 
 # https://prometheus.io/download/
 
-dlapp prometheus prometheus 2.35.0
-dlapp prometheus alertmanager 0.24.0
+#dlapp prometheus prometheus 2.35.0
+#dlapp prometheus alertmanager 0.24.0
 #dlapp prometheus blackbox_exporter 0.20.0
 #dlapp prometheus consul_exporter 0.8.0
-#dlapp prometheus graphite_exporter 0.12.0
+dlapp prometheus graphite_exporter 0.12.1
 #dlapp prometheus haproxy_exporter 0.13.0
 #dlapp prometheus memcached_exporter 0.9.0
 #dlapp prometheus mysqld_exporter 0.14.0
 #dlapp prometheus node_exporter 1.3.1
 #dlapp prometheus pushgateway 1.4.2
-#dlapp prometheus statsd_exporter 0.22.4
+dlapp prometheus statsd_exporter 0.22.5
 #dlapp prometheus-community bind_exporter 0.5.0
